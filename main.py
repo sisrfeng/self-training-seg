b4194ec2ab0d7bb22e16aa5b3bd6ca2638105a9a
good_pen <5649701+llwwff@user.noreply.gitee.com>
2022-05-31 20:26:18 +0800

    准备换名字

--- main.py
+++ main.py
@@ -25,31 +25,33 @@ MODE = None
 
 def parse_args():
     parser = argparse.ArgumentParser(description='ST and ST++ Framework')
+    arG = parser.add_argument
 
     # basic settings
-    parser.add_argument('--data-root', type=str, required=True)
-    parser.add_argument('--dataset', type=str, choices=['pascal', 'cityscapes'], default='pascal')
-    parser.add_argument('--batch-size', type=int, default=16)
-    parser.add_argument('--lr', type=float, default=None)
-    parser.add_argument('--epochs', type=int, default=None)
-    parser.add_argument('--crop-size', type=int, default=None)
-    parser.add_argument('--backbone', type=str, choices=['resnet50', 'resnet101'], default='resnet50')
-    parser.add_argument('--model', type=str, choices=['deeplabv3plus', 'pspnet', 'deeplabv2'],
+    arG('--data-root'  , type=str   , required=True)
+    arG('--dataset'    , type=str   , choices=['pascal', 'cityscapes'], default='pascal')
+    arG('--batch-size' , type=int   , default=16)
+    arG('--lr'         , type=float , default=None)
+    arG('--epochs'     , type=int   , default=None)
+    arG('--crop-size'  , type=int   , default=None)
+    arG('--backbone'   , type=str   , choices=['resnet50', 'resnet101'], default='resnet50')
+    arG('--model'      , type=str   , choices=['deeplabv3plus', 'pspnet', 'deeplabv2'],
                         default='deeplabv3plus')
 
     # semi-supervised settings
-    parser.add_argument('--labeled-id-path', type=str, required=True)
-    parser.add_argument('--unlabeled-id-path', type=str, required=True)
-    parser.add_argument('--pseudo-mask-path', type=str, required=True)
+    arG('--labeled-id-path'   , type=str , required=True)
+    arG('--unlabeled-id-path' , type=str , required=True)
+    arG('--pseudo-mask-path'  , type=str , required=True)
 
-    parser.add_argument('--save-path', type=str, required=True)
+    arG('--save-path', type=str, required=True)
 
     # arguments for ST++
-    parser.add_argument('--reliable-id-path', type=str)
-    parser.add_argument('--plus', dest='plus', default=False, action='store_true',
+    arG('--reliable-id-path', type=str)
+    arG('--plus', dest='plus', default=False, action='store_true',
                         help='whether to use ST++')
 
     args = parser.parse_args()
+    print(f'{args= }')
     return args
 
 
@@ -58,31 +60,60 @@ def main(args):
         os.makedirs(args.save_path)
     if not os.path.exists(args.pseudo_mask_path):
         os.makedirs(args.pseudo_mask_path)
-    if args.plus and args.reliable_id_path is None:
+    if args.plus and    args.reliable_id_path is None:
         exit('Please specify reliable-id-path in ST++.')
 
     criterion = CrossEntropyLoss(ignore_index=255)
 
-    valset = SemiDataset(args.dataset, args.data_root, 'val', None)
-    valloader = DataLoader(valset, batch_size=4 if args.dataset == 'cityscapes' else 1,
-                           shuffle=False, pin_memory=True, num_workers=4, drop_last=False)
+    valset = SemiDataset(args.dataset,
+                         args.data_root,
+                         'val',
+                         None,
+                        )
+    valloader = DataLoader(valset,
+                           batch_size=4 if args.dataset == 'cityscapes' else 1,
+                           shuffle     = False ,
+                           pin_memory  = True  ,
+                           num_workers = 4     ,
+                           drop_last   = False ,
+                          )
 
     # <====================== Supervised training with labeled images (SupOnly) ======================>
     print('\n================> Total stage 1/%i: '
-          'Supervised training on labeled images (SupOnly)' % (6 if args.plus else 3))
+          'Supervised training on labeled images (Supervised Only)' % (6 if args.plus else 3))
 
     global MODE
     MODE = 'train'
 
-    trainset = SemiDataset(args.dataset, args.data_root, MODE, args.crop_size, args.labeled_id_path)
-    trainset.ids = 2 * trainset.ids if len(trainset.ids) < 200 else trainset.ids
-    trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True,
-                             pin_memory=True, num_workers=16, drop_last=True)
+    trainset = SemiDataset(args.dataset,
+                           args.data_root,
+                           MODE,
+                           args.crop_size,
+                           args.labeled_id_path,
+                          )
+    trainset.ids = 2 * trainset.ids \
+                        if len(trainset.ids) < 200 \
+                        else \
+                   trainset.ids
+
+    trainloader = DataLoader(trainset,
+                             batch_size  = args.batch_size ,
+                             shuffle     = True            ,
+                             pin_memory  = True            ,
+                             num_workers = 16              ,
+                             drop_last   = True            ,
+                            )
 
     model, optimizer = init_basic_elems(args)
     print('\nParams: %.1fM' % count_params(model))
 
-    best_model, checkpoints = train(model, trainloader, valloader, criterion, optimizer, args)
+    best_model, checkpoints = train(model,
+                                    trainloader,
+                                    valloader,
+                                    criterion,
+                                    optimizer,
+                                    args,
+                                   )
 
     """
         ST framework without selective re-training
